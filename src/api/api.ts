4124de5b499fbb2adaa830f609222a4a3028ee24
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2024-11-06 15:18:03 +0100

    feat:add minor fixes to code

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,17 +2,20 @@ import { joke } from "../components/Joke";
 const path = import.meta.env.BACKEND_PATH;
 
 export const fetchJokes = async (): Promise<joke[]> => {
-    const response = await fetch(`${path}/api/jokes`);
-    return response.json();
-}
+  const response = await fetch(`${path}/api/jokes`);
+  return response.json();
+};
 
-export const postjokes = async (email: string, joke: string): Promise<joke> => {
-    const response = await fetch(`${path}/api/jokes`, {
-        method: "POST",
-        headers: {
-            "content-type": "application/JSON",
-        },
-        body: JSON.stringify({ joke, email }),
-    });
-    return response.json();
-}
+export const postjokes = async (
+  email: string | undefined,
+  joke: string
+): Promise<joke> => {
+  const response = await fetch(`${path}/api/jokes`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/JSON",
+    },
+    body: JSON.stringify({ joke, email }),
+  });
+  return response.json();
+};
