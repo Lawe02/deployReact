d716ff4b3e25550706d7b4fa99a78b9f241481ab
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2024-11-13 10:51:35 +0100

    refa refactor import in main component

--- src/components/JokeMain.tsx
+++ src/components/JokeMain.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { joke } from "./Joke";
 import JokeCard from "./JokeCard";
 import { fetchJokes } from "../api/api";
